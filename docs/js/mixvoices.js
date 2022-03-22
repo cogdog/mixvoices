84c99071ca2c69c5a369464cc776260cab3f8161
CogDog <cogdogblog@gmail.com>
2022-03-22 14:56:25 -0600

    replay link add
    Add to the voices.txt generation file the replay link

--- docs/js/mixvoices.js
+++ docs/js/mixvoices.js
@@ -10,57 +10,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const myPlaylist = urlParams.get('playlist');
 
- 
-var people = [
-	{"id" : "ajita" , "name" : "Ajita D", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]}, 
-	{"id" : "shannon" , "name" : "Shannon S", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]}, 
-	{"id" : "judith" , "name" : "Judith S", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "rajiv" , "name" : "Rajiv J", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "alex" , "name" : "Alex E", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "lena" , "name" : "Lena P", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "lori-beth" , "name" : "Lori-Beth L", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "cynthia" , "name" : "Cynthia O", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "shinta" , "name" : "Shinta H", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "alan" , "name" : "Alan L", "segments" : [2]},
-	{"id" : "marcela" , "name" : "Marcela M", "segments" : [2]},
-	{"id" : "una" , "name" : "Una D", "segments" : [2]},
-	{"id" : "paul" , "name" : "Paul S", "segments" : [2]},
-	{"id" : "igor" , "name" : "Igor L", "segments" : [2]},
-	{"id" : "kristin" , "name" : "Kristin H", "segments" : [1,2,3,4,5,6,7]},
-	{"id" : "joran" , "name" : "Jöran M-M", "segments" : [8,9,10,11,12,13,14]},
-	{"id" : "kevin" , "name" : "Kevin H", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "isla" , "name" : "Isla H-F", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "maren" , "name" : "Maren D", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "clint" , "name" : "Clint L", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-	{"id" : "nicole" , "name" : "Nicole N", "segments" : [1,2,3,4,5,6,7]},
-	{"id" : "verena" , "name" : "Verna R", "segments" : [8,9,10,11,12,13,14]},
-	{"id" : "gino" , "name" : "Gino F", "segments" : [1,2,3,4,6,7]},
-	{"id" : "werner" , "name" : "Werner W", "segments" : [8,9,10,11,12,13,14]},
-	{"id" : "bonni" , "name" : "Bonni S", "segments" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
-];
-
-// the text for each segment, will be displayed under speakers name as it plays
-var segment_text = [
-	"Hello and welcome to OEG Voices",
-	"OEG Voices",
-	"OEG Voices",
-	"OEG Voices",
-	 "a podcast bringing to you the voices and ideas of open educators from around the world",
-	 "OEG Voices is produced by Open Education Global",
-	 "a member-based, non-profit organization",
-	 "supporting the development and use of open education globally",
-	 "Learn more about us at oeglobal.org",
-	 "There’s much to take in at a global level",
-	 "We hope to bring you closer to how open education is working", 
-	 "by hearing the stories of practitioners",
-	 "told in their own voices",
-	 "Each episode introduces you to a global open educator",
-	 "and we invite you to later engage in conversation with them",
-	 "in our OEG Connect community"
-]
-
-var numsegments = 14; // number of segments in this mix
-
 // lame I know, global variables. Sue me. Initialize!
 
 
@@ -131,7 +80,7 @@ function mixaudio(max) {
 	let credits = [];
 	let ffmpegtxt = '# OEG Voices Random Intro created ' + new Date() + "\n" + 
 					'# Run from directory containing \'audio\'' + "\n" + 
-					'# ffmpeg -f concat -safe 0 -i voices.txt -c copy voicemix.mp3' + "\n" ;
+					'# ffmpeg -f concat -safe 0 -i voices.txt -c copy voicemix.mp3' + "\n";
 	paused = false;
 	speakers = [];
 
@@ -181,7 +130,8 @@ function mixaudio(max) {
 	}
 	
 	// append the credits to the download content
-	ffmpegtxt += '# Voice credits: ' + credits.join(", ");
+	ffmpegtxt += '# Playback link: ' + getReplayLink(ids)   +  "\n" 
+					+'# Voice credits: ' + credits.join(", ");
 		
 	$( "#credits" ).html('<h2>Voice Credits For This Mix</h2><p>' + credits.join(", ") + '</p>');
 	$( "#voicelist" ).text(ffmpegtxt);
@@ -199,7 +149,7 @@ function setUpReplay(max) {
 	let credits = [];
 	let ffmpegtxt = '# OEG Voices Random Intro created ' + new Date() + "\n" + 
 					'# Run from directory containing \'audio\'' + "\n" + 
-					'# ffmpeg -f concat -safe 0 -i voices.txt -c copy voicemix.mp3' + "\n" ;
+					'# ffmpeg -f concat -safe 0 -i voices.txt -c copy voicemix.mp3' + "\n";
 	paused = false;
 	speakers = [];
 	
@@ -253,7 +203,8 @@ function setUpReplay(max) {
 	}
 	
 	// append the credits to the download content
-	ffmpegtxt += '# Voice credits: ' + credits.join(", ");
+	ffmpegtxt += '# Playback link: ' + getReplayLink(ids)   +  "\n" 
+					+'# Voice credits: ' + credits.join(", ");
 		
 	$( "#credits" ).html('<h2>Voice Credits For This Mix</h2><p>' + credits.join(", ") + '</p>');
 	$( "#voicelist" ).text(ffmpegtxt);
